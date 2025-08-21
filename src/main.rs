d0ddd8e2add310ea635288e79c4fb1e287c977a5
Elijah Massey <emassey0135@gmail.com>
2025-08-21 17:56:40 -0500

    Run the GRPC server currently with all functions unimplemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,48 @@
 use evdev::{enumerate, EventSummary};
 use evdev::uinput::VirtualDevice;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{transport::Server, Request, Response, Status};
+use btspeak_key_interceptor::btspeak_key_interceptor_server::{BtspeakKeyInterceptor, BtspeakKeyInterceptorServer};
+use btspeak_key_interceptor::{Empty, BrailleKeyCombination, BrailleKeyCombinations, BrailleKeyEvent, BrailleKeyEvents};
+pub mod btspeak_key_interceptor {
+  tonic::include_proto!("btspeak_key_interceptor");
+}
+#[derive(Debug, Default)]
+pub struct MyServer {}
+#[tonic::async_trait]
+impl BtspeakKeyInterceptor for MyServer {
+  type GrabKeyCombinationsStream = ReceiverStream<Result<BrailleKeyCombination, Status>>;
+  async fn grab_key_combinations(&self, request: Request<Empty>) -> Result<Response<Self::GrabKeyCombinationsStream>, Status> {
+    let (tx, rx) = mpsc::channel(32);
+    Ok(Response::new(ReceiverStream::new(rx)))
+  }
+  type GrabKeyEventsStream = ReceiverStream<Result<BrailleKeyEvent, Status>>;
+  async fn grab_key_events(&self, request: Request<Empty>) -> Result<Response<Self::GrabKeyEventsStream>, Status> {
+    let (tx, rx) = mpsc::channel(32);
+    Ok(Response::new(ReceiverStream::new(rx)))
+  }
+  async fn set_excluded_key_combinations(&self, request: Request<BrailleKeyCombinations>) -> Result<Response<Empty>, Status> {
+    let reply = Empty {};
+    Ok(Response::new(reply))
+  }
+  async fn set_excluded_key_events(&self, request: Request<BrailleKeyEvents>) -> Result<Response<Empty>, Status> {
+    let reply = Empty {};
+    Ok(Response::new(reply))
+  }
+  async fn release_keyboard(&self, request: Request<Empty>) -> Result<Response<Empty>, Status> {
+    let reply = Empty {};
+    Ok(Response::new(reply))
+  }
+  async fn send_key_combination(&self, request: Request<BrailleKeyCombination>) -> Result<Response<Empty>, Status> {
+    let reply = Empty {};
+    Ok(Response::new(reply))
+  }
+  async fn send_key_event(&self, request: Request<BrailleKeyEvent>) -> Result<Response<Empty>, Status> {
+    let reply = Empty {};
+    Ok(Response::new(reply))
+  }
+}
 #[tokio::main]
 async fn main() {
   let mut device = enumerate()
@@ -15,7 +58,8 @@ async fn main() {
     .build()
     .unwrap();
   let mut event_stream = device.into_event_stream().unwrap();
-  loop {
+//  let (keycode_tx, mut keycode_rx) = mpsc::channel(32);
+  tokio::spawn(async move {
     while let Ok(event) = event_stream.next_event().await {
       match event.destructure() {
         EventSummary::Key(_, code, value) => {
@@ -25,5 +69,11 @@ async fn main() {
         _ => {}
       };
     };
-  };
+  });
+  let addr = "127.0.0.1:54123".parse().unwrap();
+  let server = MyServer::default();
+  Server::builder()
+    .add_service(BtspeakKeyInterceptorServer::new(server))
+    .serve(addr)
+    .await.unwrap();
 }
